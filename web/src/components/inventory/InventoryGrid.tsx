e56248c0ef9d780eeaaa8e22fd6c5e2671957387
Solareon <769465+solareon@users.noreply.github.com>
2024-07-07 09:39:11 +0200

    fix(ui): enable scrolling inventory

--- web/src/components/inventory/InventoryGrid.tsx
+++ web/src/components/inventory/InventoryGrid.tsx
@@ -7,8 +7,6 @@ import { useAppSelector } from '../../store';
 import { selectLeftInventory } from '../../store/inventory';
 import { useIntersection } from '../../hooks/useIntersection';
 
-const PAGE_SIZE = 30;
-
 const InventoryGrid: React.FC<{ inventory: Inventory, direction: 'left' | 'right' }> = ({ inventory, direction }) => {
   const weight = React.useMemo(
     () => (inventory.maxWeight !== undefined ? Math.floor(getTotalWeight(inventory.items) * 1000) / 1000 : 0),
@@ -41,16 +39,18 @@ const InventoryGrid: React.FC<{ inventory: Inventory, direction: 'left' | 'right
         </div>
         <div className={direction === 'left' ? 'inventory-grid-container-left' : 'inventory-grid-container-right'} ref={containerRef}>
           <>
-            {inventory.items.slice((isPlayerInventory && direction === 'left') ? 5 : 0, (page + 1) * PAGE_SIZE).map((item, index) => (
-              <InventorySlot
+            {inventory.items.map((item, index) => {
+                if(index < 5 && inventory.type==='player') {
+                  return ''
+                }
+                return <InventorySlot
                 key={`${inventory.type}-${inventory.id}-${item.slot}`}
                 item={item}
-                ref={index === (page + 1) * PAGE_SIZE - 1 ? ref : null}
                 inventoryType={inventory.type}
                 inventoryGroups={inventory.groups}
                 inventoryId={inventory.id}
               />
-            ))}
+            })}
           </>
         </div>
       </div>
